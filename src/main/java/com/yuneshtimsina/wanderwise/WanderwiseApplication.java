abd99ca84400af3bf135e05908f62d24e723b313
Yunesh Timsina <yuneshtimsina@gmail.com>
2025-07-11 17:32:22 +0545

    Mission 1: Spring Boot + MySQL setup with ping endpoint

---
+++ src/main/java/com/yuneshtimsina/wanderwise/WanderwiseApplication.java
@@ -0,0 +1,13 @@
+package com.yuneshtimsina.wanderwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanderwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WanderwiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuneshtimsina/wanderwise/WanderwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuneshtimsina.wanderwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanderwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
